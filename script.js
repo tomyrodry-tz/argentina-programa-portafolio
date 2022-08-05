8da218f12d2b4b4970e0619695051a56a536ddfc
Tomas Rodriguez <tomyrodry.tz@gmail.com>
2022-08-05 01:49:24 -0300

    programacion con margin, agregue iconos

---
+++ script.js
@@ -0,0 +1,8 @@
+  function myFunction() {
+    var discord = document.getElementById("mydiscord");
+    discord.classList.toggle("show");
+  }
+  function myFunction2() {
+    var discord = document.getElementById("myemail");
+    discord.classList.toggle("show");
+  }
