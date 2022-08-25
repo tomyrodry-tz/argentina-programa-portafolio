0196b8f2fe91fc6f3735e9dd09318ca79f8a3f4a
Tomas Rodriguez <tomyrodry.tz@gmail.com>
2022-08-24 23:12:13 -0300

    Termine la base de la pagina

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-  function myFunction() {
-    var discord = document.getElementById("mydiscord");
-    discord.classList.toggle("show");
-  }
-  function myFunction2() {
-    var discord = document.getElementById("myemail");
-    discord.classList.toggle("show");
-  }
+const alertaEdicion = document.getElementById('alertaEdicion')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertaEdicion.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('editarBloqueado')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Para editar primero debes logearte', 'danger')
+  })
+}
